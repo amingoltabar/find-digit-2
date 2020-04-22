7877be43d4c10a7172f07fbb3b4d7e6d17fd3042
amingoltabar <61889677+amingoltabar@users.noreply.github.com>
2020-04-22 16:24:18 +0430

    Add files via upload

--- p47.py
+++ p47.py
@@ -1,19 +1,44 @@
+def receive():#Function for inputting integer
+    count=0
+    while count==0:
+        a=input('Enter the integer :')
+        try:
+            val=int(a)
+            count+=1
+            return val
+        except ValueError:
+            try:
+                val=float(a)
+                print('You entered a float. ')
+            except ValueError:
+                print('You did not enter a number. ')
 my_list=[]
-for i in range(7):#creating the list
-    a=float(input('Create your list.Enter the integer: '))
-    while a!=int(a):
-        a=float(input('You did not enter an integer.Enter the integer: '))
-    a=int(a)
-    my_list.append(a)
-x=float(input('Enter the digit you want to find the number of its repetition in a list: '))
-while x<0 or x>9 or x!=int(x):#Entered indexes must be integer and in the range of the list.
-    x=float(input('Number you have entered is not a digit.Enter the digit again: '))
-x=int(x)
+count=1
+print('Creating the list')
+while len(my_list)<7: #creating the list
+    my_list.append(receive())
+count_1=0 
+while count_1==0:#inputting digit
+    try:
+        print('Enter the digit you want to find its number of repetitions')
+        x=input(':')
+        try:
+            val=int(x)
+            if val<=9 and val>=0:
+                count_1+=1
+            else:
+                print('You did not enter a digit.')
+        except ValueError:
+            try:
+                val=float(x)
+                print('You entered a float.')
+            except ValueError:
+                print('You did not enter a number.')
 def find_digit_in_array(x,array):#This function receives a list and a digit.Then,returns the number of repetition of this digit in the list.
     def find_digit_in_element(x,y):#This function  is defined in another function.It returns the number of repetition of the digit in the element.
-        count_1=0#A variable for counting the number of repetition of a digit in the element
+        count_1=0  #A variable for counting the number of repetition of a digit in the element
         if y==0 and x==0:
-            count_1=1
+            count_1+=1
         else:
             while(y!=0):
                 r=y%10
@@ -26,8 +51,8 @@ def find_digit_in_array(x,array):#This function receives a list and a digit.Then
     for i in range(size):
         count+=find_digit_in_element(x,array[i])
     return count
-ans=find_digit_in_array(x,my_list)
-print('Digit',x,'is repeated',ans,'times in the list.')
+ans=find_digit_in_array(val,my_list)
+print('Digit',val,'is repeated',ans,'times in the list.')
         
                 
           
